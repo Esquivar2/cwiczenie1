ff2f88f2cf9ce746b6d13933bb82aa3e84f46286
s26294 <s26294@pjwstk.edu.pl>
2024-03-18 20:26:45 +0100

    zmieniono nazwe zmiennej i na a w petli w funkcji do liczenia sredniej

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,9 @@ static double calculateAverage (int[] tab)
 {
     if(tab == null || tab.Length == 0) return 0;
     int sum = 0;
-    foreach( int i in tab )
+    foreach( int a in tab )
     {
-        sum += i;
+        sum += a;
     }
 
     return sum / tab.Length;
