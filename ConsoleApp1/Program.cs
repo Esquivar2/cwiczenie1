171cad04654f9ddf50b5052f9f3d490ae57c0fa1
s26294 <s26294@pjwstk.edu.pl>
2024-03-18 19:20:43 +0100

    added function calculateAverage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,3 +3,18 @@ Console.WriteLine("Hello, World!");
 string name = "Robert";
 Console.WriteLine(name);
 Console.WriteLine($"Hello, {name}");
+
+static double calculateAverage (int[] tab)
+{
+    if(tab == null || tab.Length == 0) return 0;
+    int sum = 0;
+    foreach( int i in tab )
+    {
+        sum += i;
+    }
+
+    return sum / tab.Length;
+}
+
+double tmp = calculateAverage(new int[]{2, 5, 7, 3, 4, 1, 7});
+Console.WriteLine(tmp);
