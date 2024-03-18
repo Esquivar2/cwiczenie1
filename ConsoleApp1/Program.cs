822744300756c7c844384120e2080bf3f15dfb6e
s26294 <s26294@pjwstk.edu.pl>
2024-03-18 20:17:30 +0100

    added function findMax

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,3 +18,17 @@ static double calculateAverage (int[] tab)
 
 double tmp = calculateAverage(new int[]{2, 5, 7, 3, 4, 1, 7});
 Console.WriteLine(tmp);
+
+static int findMax(int[] tab)
+{
+    if (tab == null || tab.Length == 0) return 0;
+    int max = tab[0];
+    for( int i = 1; i < tab.Length; i++ )
+    {
+        if (tab[i] > max)
+        {
+            max = tab[i];
+        }
+    }
+    return max;
+}
